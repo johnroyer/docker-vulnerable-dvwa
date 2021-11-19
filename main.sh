916e3a5e6047290e218118f2a1b599753abfa5d9
Zero <john.royer@gmail.com>
2021-11-19 14:28:11 +0800

    Rewrite initial script for MySQL

--- main.sh
+++ main.sh
@@ -1,12 +1,17 @@
 #!/bin/env bash
 
-echo "Starting MariaDB ...."
-service mysql start
+usermod -d /var/lib/mysql/ mysql
 
-echo "Starting PHP7.4-fpm ...."
-service php7.4-fpm start
+service mysql start && echo "MySQL started ..."
 
-echo "Starting Nginx ...."
-service nginx start
+mysql -u root -e 'CREATE DATABASE dvwa' && \
+mysql -u root -e 'CREATE USER dvwa@localhost IDENTIFIED BY "PASSWPORD"' && \
+mysql -u root -e 'GRANT ALL PRIVILEGES ON dvwa.* to dvwa@localhost; FLUSH PRIVILEGES' && \
+echo "Database initialized ..."
+
+
+service php7.4-fpm start && echo "PHP started ..."
+service nginx start && echo "Nginx started ..."
+echo ""
 
 tail -f /var/log/nginx/access.log
