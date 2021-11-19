05ff26a3e781279eaabfd7aa188f51567a25139a
Zero <john.royer@gmail.com>
2021-11-19 14:19:14 +0800

    Set unix new line

--- config.inc.php
+++ config.inc.php
@@ -18,7 +18,7 @@ $_DVWA = array();
 $_DVWA[ 'db_server' ]   = '127.0.0.1';
 $_DVWA[ 'db_database' ] = 'dvwa';
 $_DVWA[ 'db_user' ]     = 'dvwa';
-$_DVWA[ 'db_password' ] = 'p@ssw0rd';
+$_DVWA[ 'db_password' ] = 'PASSWORD';
 $_DVWA[ 'db_port'] = '3306';
 
 # ReCAPTCHA settings
